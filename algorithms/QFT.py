8fd5023cbdcf9fa7508cad7eb1198d0921a0b1ff
3lenala <211247705+3lenala@users.noreply.github.com>
2026-04-08 00:26:12 +0200

    qaoa

--- algorithms/QFT.py
+++ algorithms/QFT.py
@@ -6,14 +6,13 @@ def QFT(qc):
     number_qubits = qc.num_qubits
     for qubit in range(number_qubits):
         qc.h(qubit)
-        for higher_qubit in range(i+1,n):
+        for higher_qubit in range(qubit+1, number_qubits):
             qc.cp(np.pi/2**(higher_qubit-qubit),higher_qubit,qubit)
     for qubit in range(number_qubits//2):
         qc.swap(number_qubits, number_qubits-number_qubits-1)
     
     qc.save_statevector()
     return qc
-
 
 if __name__ == '__main__':
     qc = QuantumCircuit(4)
